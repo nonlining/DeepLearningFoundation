066007490e876ff782f29be076e17909413eecdc
nonlining <nonlining@gmail.com>
2017-10-05 14:04:28 +0800

    update

--- Project_MNIST_Backpropagation/MNIST_project.py
+++ Project_MNIST_Backpropagation/MNIST_project.py
@@ -23,36 +23,42 @@ def main():
 
     max_value = np.amax(data['data'])
     min_value = np.amin(data['data'])
+
     X_ = normalized(data['data'], max_value, min_value)
 
 
     y_ = data['target']
 
-
     n_features = X_.shape[1]
 
+    # parameters
     n_hidden = 8
     kernel_size = (3,3)
-    # init
 
+    # init layers
     W_layer1 = np.random.normal(0, 0.1, (n_hidden, kernel_size[0]*kernel_size[1]))
-    b_layer1 = np.zeros(n_hidden)
+    b_layer1 = np.random.normal(0, 0.1, (n_hidden, ))
+
     W_layer2 = np.random.normal(0, 0.1, (n_hidden, 1))
-    b_layer2 = np.zeros(1)
+    b_layer2 = np.random.normal(0, 0.1, (1, ))
 
     # test
     #print X_[0].shape, W_layer1.shape
     #print conv(X_[0], (8,8) ,W_layer1, kernel_size, (1,1)) + b_layer1[0]
 
+    # network
     X, y = Input(), Input()
-    W1, b1 = Input(), Input()
-    W2, b2 = Input(), Input()
 
+    W1, b1 = Input(), Input()
+
+    W2, b2 = Input(), Input()
 
     conv_layer1 = Conv(X, W1, b1, (8,8), kernel_size, (1,1))
     activation_1 = Relu(conv_layer1)
 
     output = soft_max(activation_1)
+
+
 
 
     feed_dict = {
@@ -69,8 +75,8 @@ def main():
     trainables = [W1, b1, W2, b2]
     forward(graph)
 
-    print graph[6].value
-    print graph
+    #print graph[6].value
+    #print graph
 
 if __name__ == '__main__':
     main()
---
+++ Project_MNIST_Backpropagation/test.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Nonlining
+#
+# Created:     05/10/2017
+# Copyright:   (c) Nonlining 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import numpy as np
+from miniflow import *
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
