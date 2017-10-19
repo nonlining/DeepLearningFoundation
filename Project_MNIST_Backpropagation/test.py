bfce03a2cc1e8a2d8f53609566de35022632a771
nonlining <nonlining@gmail.com>
2017-10-19 17:37:36 +0800

    update

--- Project_MNIST_Backpropagation/test.py
+++ Project_MNIST_Backpropagation/test.py
@@ -16,11 +16,11 @@ np.random.seed(1)
 
 def main():
 
-    n_hidden = 8
+    n_hidden = 10
     kernel_size = (3,3)
 
-    W_layer1 = np.array([[1, 0, 1, 1, 0, 1, 1, 0, 1]])
-    b_layer1 = np.zeros(n_hidden)
+    W_layer0 = np.array([[1, 0, 1, 1, 0, 1, 1, 0, 1]])
+    b_layer1 = np.ones(n_hidden)
 
     X_ = [i+1 for i in range(64)]
     X_ = np.array(X_).reshape(-1,64)
@@ -29,16 +29,26 @@ def main():
         X_ = np.vstack((X_,X_))
 
 
-    for i in range(3):
-        W_layer1 = np.vstack((W_layer1,W_layer1))
+
+    W_layer1 = W_layer0
+    for i in range(9):
+        W_layer1 = np.vstack((W_layer1,W_layer0))
+
+    print X_.shape
+    print W_layer1.shape
+    print b_layer1.shape
 
     X = Input()
+    y = Input()
+
     W1, b1 = Input(), Input()
     conv1 = Conv(X ,W1, b1, (8,8) , kernel_size, (1,1))
     activation_1 = Relu(conv1)
+    #cost = MSE(y, activation_1)
 
     feed_dict = {
         X: X_,
+        #y: y_,
         W1: W_layer1,
         b1: b_layer1,
     }
@@ -48,8 +58,9 @@ def main():
     trainables = [W1, b1]
 
     forward_and_backward(graph)
+    print graph
 
-    #print graph[-1].value
+    print graph[-1].value.shape
 
 
 
