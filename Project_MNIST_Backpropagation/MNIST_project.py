20a6647a39d0c43e4b416da73ae23285bb594c34
nonlining <nonlining@gmail.com>
2017-11-12 23:21:00 +0800

    update

--- Project_MNIST_Backpropagation/MNIST_project.py
+++ Project_MNIST_Backpropagation/MNIST_project.py
@@ -8,56 +8,97 @@
 # Copyright:   (c) Min-Jung Wang 2017
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
-from sklearn.utils import shuffle, resample
 import numpy as np
 from miniflow import *
+import gzip
 
 np.random.seed(1)
 
+key_file = {
+    'train_img':'train-images-idx3-ubyte.gz',
+    'train_label':'train-labels-idx1-ubyte.gz',
+    'test_img':'t10k-images-idx3-ubyte.gz',
+    'test_label':'t10k-labels-idx1-ubyte.gz'
+}
+
+
+def load_img(file_name):
+    file_path = "./" + file_name
+
+    with gzip.open(file_path, 'rb') as f:
+        data = np.frombuffer(f.read(), np.uint8, offset=16)
+
+    data = data.reshape(-1, 784)
+    data = data.reshape(-1, 1, 28, 28)
+    print("image done")
+
+    return data
+
+def load_label(file_name):
+    file_path = "./" + file_name
+
+    with gzip.open(file_path, 'rb') as f:
+            labels = np.frombuffer(f.read(), np.uint8, offset=8)
+    print("load label done")
+
+    return labels
+
+def one_hot_encoding(y):
+    n_values = np.max(y) + 1
+    y = np.eye(n_values, dtype=int)[y]
+    return y
+
+
 def main():
+    # load data
+    dataset = {}
+    dataset['train_img'] =  load_img(key_file['train_img'])
+    dataset['train_label'] = load_label(key_file['train_label'])
+    dataset['test_img'] = load_img(key_file['test_img'])
+    dataset['test_label'] = load_label(key_file['test_label'])
 
-    data = load_digits()
+    X_ = normalized(dataset['train_img'])
 
-    max_value = np.amax(data['data'])
-    min_value = np.amin(data['data'])
+    y_ = one_hot_encoding(dataset['train_label'])
 
-    X_ = normalized(data['data'], max_value, min_value)
-    X_ = X_.reshape(X_.shape[0], 1, 8, 8)
-    print "Input size", X_.shape
-
-    y_ = data['target']
-    n_values = np.max(y_) + 1
-    y_ = np.eye(n_values, dtype=int)[y_]
 
     # parameters
-    fitter_numbers = 2
+    fitter_numbers = 16
     kernel_size = (3,3)
     pooling_size = (2,2)
 
     # init layers
-    W_layer1 = np.random.normal(0, 0.1, (fitter_numbers, kernel_size[0]* kernel_size[1]))
-    W_layer1 = W_layer1.reshape(fitter_numbers, 1, kernel_size[0], kernel_size[1])
-    b_layer1 = np.zeros(fitter_numbers, )
-    W_layer2 = np.random.normal(0, 0.1, (25*fitter_numbers, 10))
-    b_layer2 = np.zeros(10)
+
+    W_layer1 = np.sqrt(2.0 / 3*3) * np.random.randn(fitter_numbers, 1, kernel_size[0], kernel_size[1])
+    b_layer1 = np.zeros(fitter_numbers)
+
+    W_layer2 = np.sqrt(2.0 /16*3*3) * np.random.randn(fitter_numbers, 16, kernel_size[0], kernel_size[1])
+    b_layer2 = np.zeros(fitter_numbers)
+
+    #W_layer3 = np.sqrt(2.0 /16*3*3) * np.random.randn(fitter_numbers, 16, kernel_size[0], kernel_size[1])
+    #b_layer3 = np.zeros(fitter_numbers)
+
+    W_layer3 = np.sqrt(2.0 /16*3*3) * np.random.randn(12544, 10)
+    b_layer3 = np.zeros(10)
+
 
     # network
     X, y = Input(), Input()
-
     W1, b1 = Input(), Input()
     W2, b2 = Input(), Input()
+    W3, b3 = Input(), Input()
 
-    conv_layer1 = Conv(X, W1, b1, (1,1), 0)
+    conv_layer1 = Conv(X, W1, b1, (1,1), 1)
 
-    pooling1 = Pooling(conv_layer1, pooling_size, (1,1), 0)
+    #pooling1 = Pooling(conv_layer1, pooling_size, (1,1), 0)
 
-    activation_1 = Relu(pooling1)
+    activation_1 = Relu(conv_layer1)
 
-    dropout1 = Dropout(activation_1, 0.5)
+    conv_layer2 = Conv(activation_1, W2, b2, (1,1), 1)
 
-    linear = Linear(dropout1, W2, b2)
+    activation_2 = Relu(conv_layer2)
+
+    linear = Linear(activation_2, W3, b3)
 
     output = Softmax(linear, y)
 
@@ -67,30 +108,43 @@ def main():
         W1: W_layer1,
         b1: b_layer1,
         W2:W_layer2,
-        b2:b_layer2
+        b2:b_layer2,
+        W3:W_layer3,
+        b3:b_layer3
     }
 
     graph = topological_sort(feed_dict)
 
-    trainables = [W1, b1, W2, b2]
+    trainables = [W1, b1, W2, b2, W3, b3]
     epochs = 10
     learning_rate=1e-2
+    train_size = X_.shape[0]
+    batch_size = 100
+
+    steps_per_epoch = int(train_size/batch_size)
+
+    loss_list = []
 
     for i in range(epochs):
+        loss = 0
+        for j in range(steps_per_epoch):
 
-        forward_and_backward(graph)
-        #forward(graph)
-        for t in trainables:
-            partial = t.gradients[t]
-            t.value -= learning_rate * partial
+            batch_mask = np.random.choice(train_size, batch_size)
+            X_batch = X_[batch_mask]
+            y_batch = y_[batch_mask]
 
-        loss = graph[-1].diff
-        print loss
-    print W1.value
+            X.value = X_batch
+            y.value = y_batch
 
-    #plt.figure()
-    #plt.plot(loss_list)
-    #plt.show()
+
+            forward_and_backward(graph)
+            sgd_update(trainables)
+            print(graph[-1].loss)
+
+        print("Epoch: {}, Loss: {:.3f}".format(i+1, loss/steps_per_epoch))
+
+        loss_list.append(loss/steps_per_epoch)
+
 
 
 if __name__ == '__main__':
